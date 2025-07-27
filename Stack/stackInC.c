46c74fee274227a39c469146eb758033cea49afe
omar_walied <zaza03455@gmail.com>
2025-07-27 18:52:18 +0300

    Add stack implementation in C with push, pop, and utility functions

---
+++ Stack/stackInC.c
@@ -0,0 +1,88 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define STACKSIZE 100
+#define INT       1
+#define FLOAT     2
+#define STRING    3
+
+// Defined but currently unused
+struct stackelement {
+	int etype; // INT, FLOAT, or STRING
+	union {
+		int ival;
+		float fval;
+		char *pval; // Pointer to a string
+	} element;
+};
+
+struct stack {
+	int top;
+	int items[STACKSIZE];
+};
+
+int empty(struct stack *s) {
+	return s->top == -1;
+}
+
+int pop(struct stack *s) {
+	if (empty(s)) {
+		printf("Stack underflow\n");
+		exit(EXIT_FAILURE);
+	}
+	return s->items[s->top--];
+}
+
+void popandtest(struct stack *s, int *x, int *isund) {
+	if (empty(s)) {
+		*isund = 1;
+	} else {
+		*isund = 0;
+		*x = s->items[s->top--];
+	}
+}
+
+void push(struct stack *s, int val) {
+	if (s->top == STACKSIZE - 1) {
+		printf("Stack overflow\n");
+		exit(EXIT_FAILURE);
+	}
+	s->items[++s->top] = val;
+}
+
+void pushandtest(struct stack *s, int x, int *overflow) {
+	if (s->top == STACKSIZE - 1) {
+		*overflow = 1;
+	} else {
+		*overflow = 0;
+		s->items[++s->top] = x;
+	}
+}
+
+int top(struct stack *s) {
+	if (empty(s)) {
+		printf("Stack underflow\n");
+		exit(EXIT_FAILURE);
+	}
+	return s->items[s->top];
+}
+
+int main() {
+	struct stack s;
+	s.top = -1;
+
+	push(&s, 10);
+	push(&s, 20);
+
+	int val = pop(&s);
+	printf("Popped: %d\n", val);
+
+	int testVal, isUnderflow;
+	popandtest(&s, &testVal, &isUnderflow);
+	if (!isUnderflow)
+		printf("Popandtest got: %d\n", testVal);
+	else
+		printf("Stack underflow (popandtest)\n");
+
+	return 0;
+}
